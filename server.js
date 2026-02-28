1f79ef48d1706b4ffaefe525511e26d99bc1149e
PrasadYeole-Developer <prasadyeole7777@gmail.com>
2026-02-28 22:30:58 +0530

    (initialization): socket.io

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+const { Server } = require("socket.io");
 const app = require("./src/app");
+const { createServer } = require("http");
 const PORT = 3000;
 
-app.listen(3000, () => {
+const httpServer = createServer(app);
+
+Server.on("connection", (socket) => {});
+
+httpServer.listen(3000, () => {
   console.log(`Server is runnning on ${PORT}`);
 });
